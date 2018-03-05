960193ddf546417da0a26910d2ae1168a3227067
connor2110 <connorjardine97@gmail.com>
2018-03-05 14:28:17 +0000

    initial translation work

--- TK/app/src/main/java/com/holly/tourking/MainActivity.java
+++ TK/app/src/main/java/com/holly/tourking/MainActivity.java
@@ -1,5 +1,6 @@
 package com.holly.tourking;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -32,6 +34,14 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        final Button button = findViewById(R.id.translate);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                startActivity(new Intent(MainActivity.this, popTranslate.class));
+            }
+        });
     }
 
     @Override
