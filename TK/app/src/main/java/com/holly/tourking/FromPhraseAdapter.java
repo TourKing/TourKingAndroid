6deb166ce5d13fdcb8c684ba80be3883cdfbc5dd
LMacPhail <2183332m@gla.ac.uk>
2018-03-09 11:03:46 +0000

    suggested card displays only once

--- TK/app/src/main/java/com/holly/tourking/FromPhraseAdapter.java
+++ TK/app/src/main/java/com/holly/tourking/FromPhraseAdapter.java
@@ -1,11 +1,13 @@
 package com.holly.tourking;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -14,20 +16,35 @@ import java.util.List;
  * Created by Lucy on 08/03/2018.
  */
 
-public class FromPhraseAdapter extends RecyclerView.Adapter<FromPhraseAdapter.MyViewHolder> {
+public class FromPhraseAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private Context mContext;
     private List<Phrase> phrases;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class PhraseViewHolder extends RecyclerView.ViewHolder {
+        public CardView cv;
         public TextView phrase, translation;
         public ImageButton speakButton;
 
-
-        public MyViewHolder(View view) {
+        public PhraseViewHolder(View view) {
             super(view);
             phrase = (TextView) view.findViewById(R.id.phrase);
             translation = (TextView) view.findViewById(R.id.translation);
             speakButton = (ImageButton) itemView.findViewById(R.id.SpeakButton);
+        }
+
+
+    }
+    public class SuggestedViewHolder extends RecyclerView.ViewHolder {
+        public CardView cv;
+        public TextView phrase, translation, quick;
+        public ImageView image;
+
+        public SuggestedViewHolder(View view) {
+            super(view);
+            phrase = (TextView) view.findViewById(R.id.phrase2);
+            translation = (TextView) view.findViewById(R.id.translation2);
+            quick = (TextView) view.findViewById(R.id.quick);
+            image = (ImageView) view.findViewById(R.id.museum);
         }
     }
 
@@ -37,28 +54,52 @@ public class FromPhraseAdapter extends RecyclerView.Adapter<FromPhraseAdapter.My
         this.phrases = phrases;
     }
 
-    @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.phrases_page, parent, false);
+    private final int STATIC_CARD = 0;
+    private final int DYNAMIC_CARD = 1;
 
-        return new MyViewHolder(itemView);
+    @Override
+    public int getItemViewType(int position) {
+        if(position == 0) {
+            return STATIC_CARD;
+        } else {
+            return DYNAMIC_CARD;
+        }
     }
 
     @Override
-    public void onBindViewHolder(final MyViewHolder holder, int position) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if(getItemViewType(viewType) == STATIC_CARD){
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.suggested_card, parent, false);
+            return new SuggestedViewHolder(view);
+        } else{
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.phrases_page, parent, false);
+            return new PhraseViewHolder(itemView);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
         final Phrase phrase = phrases.get(position);
-        holder.phrase.setText(phrase.translation);
-        holder.translation.setText(phrase.phrase);
-        holder.speakButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                MainActivity.beenWaiting(phrase.phrase, 0);
-            }
-        });
+
+        if (getItemViewType(position) == DYNAMIC_CARD) {
+            PhraseViewHolder mHolder = (PhraseViewHolder) holder;
+
+            mHolder.phrase.setText(phrase.translation);
+            mHolder.translation.setText(phrase.phrase);
+            mHolder.speakButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MainActivity.beenWaiting(phrase.translation, 1);
+                }
+            });
+        }
     }
+
 
     @Override
     public int getItemCount() {
         return phrases.size();
     }
+
 }
--- TK/app/src/main/java/com/holly/tourking/ToPhraseAdapter.java
+++ TK/app/src/main/java/com/holly/tourking/ToPhraseAdapter.java
@@ -1,11 +1,13 @@
 package com.holly.tourking;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,19 +21,35 @@ import java.util.List;
  * Created by Lucy on 08/03/2018.
  */
 
-public class ToPhraseAdapter extends RecyclerView.Adapter<ToPhraseAdapter.MyViewHolder> {
+public class ToPhraseAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private Context mContext;
     private List<Phrase> phrases;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class PhraseViewHolder extends RecyclerView.ViewHolder {
+        public CardView cv;
         public TextView phrase, translation;
         public ImageButton speakButton;
 
-        public MyViewHolder(View view) {
+        public PhraseViewHolder(View view) {
             super(view);
             phrase = (TextView) view.findViewById(R.id.phrase);
             translation = (TextView) view.findViewById(R.id.translation);
             speakButton = (ImageButton) itemView.findViewById(R.id.SpeakButton);
+        }
+
+
+    }
+    public class SuggestedViewHolder extends RecyclerView.ViewHolder {
+        public CardView cv;
+        public TextView phrase, translation, quick;
+        public ImageView image;
+
+        public SuggestedViewHolder(View view) {
+            super(view);
+            phrase = (TextView) view.findViewById(R.id.phrase2);
+            translation = (TextView) view.findViewById(R.id.translation2);
+            quick = (TextView) view.findViewById(R.id.quick);
+            image = (ImageView) view.findViewById(R.id.museum);
         }
     }
 
@@ -41,24 +59,48 @@ public class ToPhraseAdapter extends RecyclerView.Adapter<ToPhraseAdapter.MyView
         this.phrases = phrases;
     }
 
+    private final int STATIC_CARD = 0;
+    private final int DYNAMIC_CARD = 1;
+
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.phrases_page, parent, false);
-        return new MyViewHolder(itemView);
+    public int getItemViewType(int position) {
+        if(position == 0) {
+            return STATIC_CARD;
+        } else {
+            return DYNAMIC_CARD;
+        }
     }
 
     @Override
-    public void onBindViewHolder(final MyViewHolder holder, int position) {
-        final Phrase phrase = phrases.get(position);
-        holder.phrase.setText(phrase.phrase);
-        holder.translation.setText(phrase.translation);
-        holder.speakButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                MainActivity.beenWaiting(phrase.translation, 1);
-            }
-        });
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if(getItemViewType(viewType) == STATIC_CARD){
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.suggested_card, parent, false);
+            return new SuggestedViewHolder(view);
+        } else{
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.phrases_page, parent, false);
+            return new PhraseViewHolder(itemView);
+        }
     }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        final Phrase phrase = phrases.get(position);
+
+        if (getItemViewType(position) == DYNAMIC_CARD) {
+            PhraseViewHolder mHolder = (PhraseViewHolder) holder;
+
+            mHolder.phrase.setText(phrase.phrase);
+            mHolder.translation.setText(phrase.translation);
+            mHolder.speakButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MainActivity.beenWaiting(phrase.translation, 1);
+                }
+            });
+        }
+    }
+
 
     @Override
     public int getItemCount() {
